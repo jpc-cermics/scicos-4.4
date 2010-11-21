cfd51495ce8f124aab82eb2b3388d9880c6994f6
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2010-11-21 22:03:18 +0000

    updates

--- src/blocks/bit_clear_8.c
+++ src/blocks/bit_clear_8.c
@@ -1,6 +1,6 @@
 #include "blocks.h"
 
-void bit_clear_8 (scicos_block * block, int flag)
+void bit_clear_8_old (scicos_block * block, int flag)
 {
   int n, m, i;
   char *opar;
@@ -13,3 +13,23 @@ void bit_clear_8 (scicos_block * block, int flag)
   for (i = 0; i < m * n; i++)
     *(y + i) = ((*(u + i)) & (*opar));
 }
+
+#define BIT_CLEAR(tag, type)						\
+  void CNAME(new_bit_clear_,tag) (scicos_block * block, int flag)		\
+  {									\
+    int i;								\
+    int m= GetInPortRows (block, 1);					\
+    int n= GetInPortCols (block, 1);					\
+    type *opar = (type *) GetOparPtrs (block, 1);			\
+    type *u =(type *) GetInPortPtrs (block, 1);				\
+    type *y =(type *) GetOutPortPtrs (block, 1);			\
+    for (i = 0; i < m * n; i++)						\
+      *(y + i) = ((*(u + i)) & (*opar));				\
+  }
+
+BIT_CLEAR(8, gint8)
+BIT_CLEAR(16, gint16)
+BIT_CLEAR(32, gint)
+
+#undef BIT_CLEAR 
+
--- src/blocks/bit_set_8.c
+++ src/blocks/bit_set_8.c
@@ -13,3 +13,23 @@ void bit_set_8 (scicos_block * block, int flag)
   for (i = 0; i < m * n; i++)
     *(y + i) = ((*(u + i)) | (*opar));
 }
+
+#define BIT_SET(tag, type)						\
+  void CNAME(new_bit_set_,tag) (scicos_block * block, int flag)		\
+  {									\
+    int i;								\
+    int m= GetInPortRows (block, 1);					\
+    int n= GetInPortCols (block, 1);					\
+    type *opar = (type *) GetOparPtrs (block, 1);			\
+    type *u =(type *) GetInPortPtrs (block, 1);				\
+    type *y =(type *) GetOutPortPtrs (block, 1);			\
+    for (i = 0; i < m * n; i++)						\
+      *(y + i) = ((*(u + i)) | (*opar));				\
+  }
+
+BIT_SET(8, gint8)
+BIT_SET(16, gint16)
+BIT_SET(32, gint)
+
+#undef BIT_SET 
+
