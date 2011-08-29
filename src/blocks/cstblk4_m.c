489c480aff527b1b963b529933fc0ebb23e1ab96
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2011-08-29 16:58:36 +0000

    update

--- src/blocks/cstblk4_m.c
+++ src/blocks/cstblk4_m.c
@@ -1,20 +1,19 @@
 #include "blocks.h"
 
+/* Copyright INRIA
+ *   Scicos block simulator
+ *  output a vector of constants out(i)=opar(i)
+ *  opar(1:nopar) : given constants 
+ */
+
 void cstblk4_m (scicos_block * block, int flag)
 {
-  /* Copyright INRIA
-
-     Scicos block simulator
-     output a vector of constants out(i)=opar(i)
-     opar(1:nopar) : given constants */
-  int nopar, mo, no, so;
-  void *y;
-  void *opar;
-  nopar = GetNopar (block);
-  y = GetOutPortPtrs (block, 1);
-  opar = GetOparPtrs (block, 1);
-  mo = GetOparSize (block, 1, 1);
-  no = GetOparSize (block, 1, 2);
-  so = GetSizeOfOpar (block, 1);
+  /* int nopar = GetNopar (block); */
+  void *y = GetOutPortPtrs (block, 1);
+  void *opar = GetOparPtrs (block, 1);
+  int mo = GetOparSize (block, 1, 1);
+  int no = GetOparSize (block, 1, 2);
+  int so = GetSizeOfOpar (block, 1);
   memcpy (y, opar, mo * no * so);
 }
+
